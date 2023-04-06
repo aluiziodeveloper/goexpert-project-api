c93bcd45ebef3d0e5a4e2ea8ea56ca3d2e13ca5e
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-04-06 19:54:56 -0300

    Finishing config file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/aluiziodeveloper/goexpert-project-api/configs"
 
+func main() {
+	config, _ := configs.LoadConfig(".")
+	println(config.DBDriver)
 }
