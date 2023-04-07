cb7f96064e92dd6459b61ff294ed5eb064dd5696
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-04-07 14:37:44 -0300

    Creating product handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "github.com/aluiziodeveloper/goexpert-project-api/configs"
+import (
+	"github.com/aluiziodeveloper/goexpert-project-api/configs"
+	"github.com/aluiziodeveloper/goexpert-project-api/internal/entity"
+	"github.com/aluiziodeveloper/goexpert-project-api/internal/infra/database"
+	"github.com/aluiziodeveloper/goexpert-project-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(&entity.Product{}, &entity.User{})
+	if err != nil {
+		panic(err)
+	}
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	err = http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
